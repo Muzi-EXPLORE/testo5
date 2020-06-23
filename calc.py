c2a1473de83d8be497e7c9eb172c986a88936525
Muzi Xaba <muzi@explore-ai.net>
2020-06-23 19:40:20 +0200

    update add function to raise ValueError if adding strings. fixes #6

--- calc.py
+++ calc.py
@@ -2,6 +2,8 @@ print("hello world")
 
 def adder(a, b):
     """Returns the sum of a and b"""
+    if type(a)==str or type(b)==str:
+        raise ValueError("No strings allowed")
     return a+b
 
 def subt(a, b):
